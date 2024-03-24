6a8c39b6edd1b9868b16c50fad1af878216fcb25
krossale <krossale@fel.cvut.cz>
2024-03-24 15:21:32 +0100

    fixed CSS

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,30 +1,46 @@
 // src/components/Tetris.js
 import React, { Component } from 'react';
-import Grid from './Grid';
-import MiniGrid from './MiniGrid';
 
 class Tetris extends Component {
   render() {
     return (
       <div className="board">
-        
-        <div className="score">Score: 0</div>
+        <div className="border-out">
+          <div className="border-in">
 
-        <button className="start" onClick={this.props.onClick}>
-          Start/Pause
-        </button>
+            <div className="score">Score: 0</div>
 
-        <div className="container">
-          <Grid />
-          <MiniGrid />
-        </div>
-        <div className="buttons">
-        <button id="change-btn">Change</button>
-              <div class="bottom">
-                <button id="left-btn">Left</button>
-                <button id="down-btn">Down</button>
-                <button id="right-btn">Right</button>
+            <div className="start">
+              <button  onClick={this.props.onClick}>Start/Pause</button>
+            </div>
+
+            <div className="container">
+              <div className="frame">
+                
+                <div className="grid">
+                  {Array.from(Array(200), (_, i) => (
+                    <div key={i} className="grid-square"></div>
+                  ))}
+                </div>
+
+                <div className="mini-grid">
+                  {Array.from(Array(16), (_, i) => (
+                    <div key={i} className="mini-grid-square"></div>
+                  ))}
+                </div>
+                
               </div>
+            </div>
+            <div className="buttons">
+            <button id="change-btn">Change</button>
+                  <div class="bottom">
+                    <button id="left-btn">Left</button>
+                    <button id="down-btn">Down</button>
+                    <button id="right-btn">Right</button>
+                  </div>
+            </div>
+
+          </div>
         </div>
       </div>
     );
